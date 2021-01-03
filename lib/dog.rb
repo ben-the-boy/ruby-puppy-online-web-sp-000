8dc39ceac87455de7da1b1088c448ac72f8e5a82
Ben Hayes <b.t.hayes@comcast.net>
2021-01-03 22:14:04 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.print_all
+  end 
+  
+  
+end
